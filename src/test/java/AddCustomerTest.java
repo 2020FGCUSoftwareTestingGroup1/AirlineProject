39b99b09e5a6aa910fff3440dc7929feb21ab37f
tgayle <travisgayledev@gmail.com>
2021-03-06 17:01:14 -0500

    wip: Make a test directory/temp file.

---
+++ src/test/java/AddCustomerTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AddCustomerTest {
+    @Test
+    void test() {
+        Assertions.assertTrue(true);
+    }
+}
