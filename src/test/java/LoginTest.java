761807e77fcd0aea319901eb3afbf896f032c1db
Luis Hernandez <cobolongo22@gmail.com>
2021-03-10 19:00:31 -0500

    Fixed spelling errors oscar made and added LoginTest.java

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This LoginTest test case contains the tests to ensure a valid user is logging into the software product.
+ * This test class implements the Decision Table with the following given set of inputs:
+ *
+ * - Username is Valid (T) && Password is Valid (T) - Log into the System
+ * - Username is Valid (T) && Password is Invalid (F) - Fail to login; show error message (password is incorrect)
+ * - Username is Invalid (F) && Password is Valid (T) - Fail to login; show error message (User does not exist)
+ * - Username is Invalid (F) && Password is Invalid (F) - Fail to login; show error message (incorrect username and pw)
+ *
+ * The following method
+ * @see #userNameAndPasswordReturnsTrue()
+ */
+public class LoginTest {
+
+    /**
+     * This method checks to see if the username and password match in the database, thus prompting a successful login.
+     * We do this to ensure security to our software system.
+     */
+    void userNameAndPasswordReturnsTrue() {
+
+
+    }
+    void userNameReturnsTrueButPasswordReturnsFalse(){
+
+
+    }
+    void userNameReturnsFalseButPasswordReturnsTrue(){
+
+
+    }
+    void userNameAndPasswordReturnsFalse(){
+
+
+    }
+}
