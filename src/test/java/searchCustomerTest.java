0a5fab6649f3ebdf72cd805bb31f84fc09489398
reltaher <43248170+reltaher@users.noreply.github.com>
2021-04-19 22:58:59 -0400

    searchCustomerTest: Added a test to find a valid customer in the search customer tab.

--- src/test/java/searchCustomerTest.java
+++ src/test/java/searchCustomerTest.java
@@ -1,5 +1,56 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import database.Database;
+import database.IDatabase;
+import model.Customer;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import view.Main;
+import view.searchCustomer;
+
 class searchCustomerTest {
+
+  FrameFixture window;
+
+  @Mock
+  IDatabase mockDatabase;
+
+    @BeforeEach
+    public void setup() {
+      MockitoAnnotations.openMocks(this);
+
+      Database.setDatabase(mockDatabase);
+
+      Main frame = GuiActionRunner.execute(() -> new Main());
+      window = new FrameFixture(frame);
+      window.show();
+    }
+
+    @Test
+    public void findValidCustomer() {
+      Customer customer = new Customer("CS001", "Joseph", "Madre", "1",
+          "AJ24", "123 First Street", "1/1/1990", "Male",
+          200, null);
+
+      Mockito.when(mockDatabase.getCustomer("CS001")).thenReturn(customer);
+
+      window.menuItem("customerRootMenu").click();
+      window.menuItem("searchCustomer").click();
+      window.textBox("customerIdInput").setText("CS001");
+      window.button("findButton").click();
+
+      Mockito.verify(mockDatabase).getCustomer(Mockito.any());
+    }
+
+    @AfterEach
+    void cleanup() {
+      window.cleanUp();
+    }
 
 }
