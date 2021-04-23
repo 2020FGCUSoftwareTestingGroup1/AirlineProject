49402eb07177bf48fc0c69ab313684bf9abb4895
reltaher <43248170+reltaher@users.noreply.github.com>
2021-04-22 21:47:16 -0400

    AddCustomerIntegrationTest: Added Documentation / Comments

--- src/test/java/integration/usercreation/AddCustomerIntegrationTest.java
+++ src/test/java/integration/usercreation/AddCustomerIntegrationTest.java
@@ -10,12 +10,20 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import view.Main;
 
+/**
+ * Class for each Integration Testing Method in the "Add Customer" screen.
+ */
 public class AddCustomerIntegrationTest {
     private FrameFixture window;
 
     @Mock
     IDatabase mockDatabase;
 
+    /**
+     * The setup method is ran before each test is executed. It opens a mock of the test class,
+     * sets the database to the mock database, and executes and opens a window frame which shows
+     * the main screen of the project.
+     */
     @BeforeEach
     void setup() {
         MockitoAnnotations.openMocks(this);
@@ -26,25 +34,43 @@ public class AddCustomerIntegrationTest {
         window.show();
     }
 
+    /**
+     * The teardown method is a method that is ran after each test is executed. It cleans up the
+     * window that is currently running through the test to prepare for the next test to be properly
+     * executed.
+     */
     @AfterEach
     void teardown() {
         window.cleanUp();
     }
 
+    /**
+     * An integration testing method which ensures that it displays the next customer based on their
+     * ID.
+     */
     @Test
     @Tag("ui")
     void displaysNextCustomerIdOnOpen() {
+        // Mockito Test to find a customer.
         Mockito.when(mockDatabase.getNextCustomerId()).thenReturn("CS001");
 
+        // Mockito - Executing the test: Displaying the next customer on file.
         window.menuItem("customerRootMenu").click();
         window.menuItem("addCustomerMenuItem").click();
         window.label("nextCustomerIdLabel").requireText("CS001");
     }
 
+    /**
+     * An integration testing method which shows an error when there is a missing field that the
+     * user did not fill out.
+     */
     @Test
     @Tag("ui")
     void errorShownWhenMissingField() {
+        // Mockito Test to find a customer.
         Mockito.when(mockDatabase.getNextCustomerId()).thenReturn("CS001");
+
+        //Mockito - Executing the test: Adding a customer with missing fields.
         window.menuItem("customerRootMenu").click();
         window.menuItem("addCustomerMenuItem").click();
         window.button("addCustomerAddButton").click();
